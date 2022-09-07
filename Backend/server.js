f16c9c4028e1dcf8e1f74152e2e76c17a3eb408a
Home <ishipigera@gmail.com>
2022-09-08 01:55:34 +0530

    Added Supplier backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const stock_routes = require("./routes/stockRoutes")
+const productManagerRoutes = require("./routes/productManagerRoutes")
 const app = express()
 dotenv.config()
 require ('./DB/DB.js');
@@ -13,6 +14,7 @@ app.use(express.json())
 
 //route middlewares
 app.use(stock_routes);
+app.use(productManagerRoutes);
 
 const port = process.env.PORT || 8000
 
