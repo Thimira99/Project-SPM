a408cbff6fc4a5c01413c4ca9c12ddf1f413d8ee
IT20005580 <88153854+Thimira99@users.noreply.github.com>
2022-09-09 01:46:03 +0530

    notes view and add

---
+++ frontend/src/pages/admin/Notes/createNotes.jsx
@@ -0,0 +1,98 @@
+import React, { Component, useEffect, useState } from 'react';
+
+import { useHistory } from 'react-router-dom';
+
+//import toast msg
+import { toastMsg } from '../../../toast';
+
+//import axios
+import axios from 'axios';
+
+//import add notes style
+import addNote from './createNotes.module.scss';
+import Navbar from '../../../components/Navbar';
+
+function CreateNotes() {
+
+    const history = useHistory();
+
+    //initialize variables
+    const [userId, setId] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    useEffect(() => {
+        //get id and firstname from localstorage
+        const { id } = JSON.parse(localStorage.getItem('user'));
+
+        setId(id);
+    }, [])
+
+
+    //hanlde onchange
+    function handleTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    function handleDescription(event) {
+        setDescription(event.target.value);
+    }
+
+    //handle submit
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const data = {
+            userId,
+            title,
+            description
+        }
+
+        //post
+        axios.post("http://localhost:8000/add", data).then(res => {
+            if (res.data.status === true) {
+                toastMsg("Note added Successfully")
+                history.push('/addNote')
+            }
+        }).catch(({ response }) => {
+            toastMsg(response.data.msg, 'error')
+
+        })
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div className={addNote.add_container}>
+                <div className={addNote.addform_container}>
+                    <div className={addNote.form_container}>
+                        <form className={addNote.form} onSubmit={handleSubmit}>
+                            <h1>Add Note</h1>
+                            <input
+                                type='text'
+                                placeholder='Title'
+                                name='title'
+                                onChange={handleTitle}
+                                required
+                                className={addNote.input}
+                            />
+                            <label>Description</label>
+                            <textarea
+                                rows="4"
+                                cols="50"
+                                name="comment"
+                                onChange={handleDescription}
+                                className={addNote.input}
+                                placeholder='Description'>
+
+                            </textarea>
+                            <button type='submit' className={addNote.greenBtn}>Create Note</button>
+                        </form>
+                    </div>
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default CreateNotes;
---
+++ frontend/src/pages/admin/Notes/updateNotes.jsx
@@ -0,0 +1,118 @@
+import React, { Component, useEffect, useState } from 'react';
+
+import { useHistory, useParams } from 'react-router-dom';
+
+//import bootstrap spinner
+import Spinner from 'react-bootstrap/Spinner';
+
+//import axios
+import axios from 'axios';
+
+//import update notes styles
+import updateNote from './updateNotes.module.scss';
+
+// import toast msg
+import { toastMsg } from '../toast';
+
+function UpdateNotes() {
+
+    const history = useHistory();
+
+    //get id from params
+    const { id } = useParams();
+
+    //loading spinner variable
+    const [loading, setLoading] = useState(true);
+
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+
+    useEffect(() => {
+        //get
+        axios.get(`http://localhost:8000/getById/${id}`).then(res => {
+            setLoading(false);
+            setTitle(res.data.data[0].title);
+            setDescription(res.data.data[0].description);
+        }).catch(err => {
+            alert(err)
+            setLoading(false);
+        })
+
+
+    }, [])
+
+
+    //handle onChange
+    function handleTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    function handleDescription(event) {
+        setDescription(event.target.value);
+
+    }
+
+    //handle submit
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description
+        }
+
+        //put
+        axios.put(`http://localhost:8000/updateById/${id}`, data).then(res => {
+            if (res.data.status === true) {
+                history.push("/addNote");
+                toastMsg('update successfully.');
+
+            }
+        }).catch(err => {
+            toastMsg(err.response.data.msg, 'error');
+
+        })
+    }
+
+    return (
+        <div>
+            {!loading ? <div className={updateNote.add_container}>
+                <div className={updateNote.addform_container}>
+                    <div className={updateNote.form_container}>
+                        <form className={updateNote.form} onSubmit={handleSubmit}>
+                            <h1>Update Note</h1>
+                            <input
+                                type='text'
+                                placeholder='Title'
+                                name='title'
+                                value={title}
+                                onChange={handleTitle}
+                                required
+                                className={updateNote.input}
+                            />
+                            <label>Description</label>
+                            <textarea
+                                rows="4"
+                                cols="50"
+                                name="comment"
+                                value={description}
+                                onChange={handleDescription}
+                                className={updateNote.input}
+                                placeholder='Description'>
+
+                            </textarea>
+                            <button type='submit' className={updateNote.greenBtn}>Update</button>
+                        </form>
+                    </div>
+                </div>
+            </div > : <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+                <Spinner animation="border" variant="primary" />
+            </div>}
+
+        </div>
+    )
+}
+
+export default UpdateNotes;
