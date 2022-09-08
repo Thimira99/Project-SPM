4cc956fe7610aa49a3afd4f7dcccab549e78104d
IT20005580 <88153854+Thimira99@users.noreply.github.com>
2022-09-08 09:02:54 +0530

    login ui

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import login from './Login.module.scss';
 
 function Login() {
+
+    //use History
+    const history = useHistory();
+
+    //initializing variables
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return (
         <>
-            <h1>LOGIN PAGE</h1>
+            {/* <h1>LOGIN PAGE</h1>
             <button className='btn btn-primary ' style={{ height: "40px" }}>
                 <Link to='/mainPage' style={{ color: "white" }}>
                     Main Page
@@ -14,7 +25,33 @@ function Login() {
                 <Link to='/salesPerson' style={{ color: "white" }}>
                     Sales Person
                 </Link>
-            </button>
+            </button> */}
+
+
+            <div className={login.login_container}>
+                <div className={login.loginform_container}>
+                    <div className={login.form_container}>
+                        <form className={login.form} >
+                            <h1>LogIn</h1>
+                            <input
+                                type='text'
+                                placeholder='Email'
+                                name='email'
+                                required
+                                className={login.input}
+                            />
+                            <input
+                                type='password'
+                                placeholder='Password'
+                                name='password'
+                                required
+                                className={login.input}
+                            />
+                            <button type='submit' className={login.greenBtn}>LogIn</button>
+                        </form>
+                    </div>
+                </div>
+            </div >
         </>
     )
 }
