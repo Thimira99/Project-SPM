1db263138f1115d9a14da76b7a77006a98f384eb
IT20005580 <88153854+Thimira99@users.noreply.github.com>
2022-10-13 01:20:08 +0530

    complete notes edit and delete

--- frontend/src/pages/admin/AdminProfile/admiProfile.jsx
+++ frontend/src/pages/admin/AdminProfile/admiProfile.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import Navbar from '../../../components/Navbar';
 
@@ -9,38 +8,43 @@ import jwt from 'jwt-decode';
 
 import style from './adminProfile.module.scss';
 
-function AdminProfile(){
+function AdminProfile() {
+	const user = localStorage.getItem('Token');
+	const result = jwt(user);
 
-  const user = localStorage.getItem('Token');
-  const result = jwt(user)
-
-
-  return(
-    <>
-  
-    <Navbar />
-    <div className={style.main}>
-        <div className={style.htag}> 
-          <h1 >Admin Profile </h1>
-
-        </div>
-        <div className={style.mainCard}>
-          <div class="card" style={{width:'300px'}}>
-            <img class="card-img-top" src={profileLogo} alt="Card image cap"/>
-            <div class="card-body">
-              <h5 class="card-title">{result.firstName} {result.lastName}</h5>
-              <h5>Date Of Birth : <span>{result.dateOfBirth}</span></h5>
-              <h5>Mobile : <span>{result.mobile}</span></h5>
-              <h5>Account Type : <span>{result.accountType}</span></h5>
-              <h5>Account Type : <span>{result.accountType}</span></h5>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        
-      </div>
-  </>
-  )
+	return (
+		<>
+			<Navbar />
+			<div className={style.main}>
+				<div className={style.htag}>
+					<h1>Admin Profile </h1>
+				</div>
+				<div className={style.mainCard}>
+					<div class='card' style={{ width: '300px' }}>
+						<img class='card-img-top' src={profileLogo} alt='Card image cap' />
+						<div class='card-body'>
+							<h5 class='card-title'>
+								{result.firstName} {result.lastName}
+							</h5>
+							<h5>
+								Date Of Birth : <span>{result.dateOfBirth}</span>
+							</h5>
+							<h5>
+								Mobile : <span>{result.mobile}</span>
+							</h5>
+							<h5>
+								Account Type : <span>{result.accountType}</span>
+							</h5>
+							<h5>
+								Account Type : <span>{result.accountType}</span>
+							</h5>
+							<button>Update</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default AdminProfile;
--- frontend/src/pages/admin/Notes/addNotes.jsx
+++ frontend/src/pages/admin/Notes/addNotes.jsx
@@ -11,7 +11,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 //import image
-import image from '../../../images/monophy.gif'
+import image from '../../../images/monophy.gif';
 
 //import Navbar
 import Navbar from '../../../components/Navbar';
@@ -26,92 +26,116 @@ import Spinner from 'react-bootstrap/Spinner';
 import { toastMsg } from '../../../toast';
 
 function AddNotes() {
+	//initializing loading variablr
+	const [loading, setLoading] = useState(true);
 
-    //initializing loading variablr
-    const [loading, setLoading] = useState(true);
+	const [firstName, setFirstName] = useState('');
+	// const [id, setId] = useState('');
 
+	const [notes, setNotes] = useState([]);
 
-    const [firstName, setFirstName] = useState('');
-    // const [id, setId] = useState('');
+	useEffect(() => {
+		//get id and firstname from localstorage
+		const { id, firstName } = JSON.parse(localStorage.getItem('user'));
 
-    const [notes, setNotes] = useState([]);
+		setFirstName(firstName);
 
-    useEffect(() => {
-        //get id and firstname from localstorage
-        const { id, firstName } = JSON.parse(localStorage.getItem('user'));
+		//get notes by student id
+		axios
+			.get(`http://localhost:8000/getByUserId/${id}`)
+			.then((res) => {
+				if (res.data.data.length === 0) {
+					toastMsg('You Dont Have Notes', 'info');
+				} else {
+					setNotes(res.data.data);
+				}
+				setLoading(false);
+			})
+			.catch(({ response }) => {
+				setLoading(false);
 
-        setFirstName(firstName);
+				toastMsg(response.data.msg, 'error');
+			});
+	}, []);
 
-        //get notes by student id
-        axios.get(`http://localhost:8000/getByUserId/${id}`).then(res => {
-            if (res.data.data.length === 0) {
-                toastMsg("You Dont Have Notes", 'info')
-            } else {
-                setNotes(res.data.data);
-            }
-            setLoading(false);
-        }).catch(({ response }) => {
-            setLoading(false);
+	function handleDelete(id) {
+		setLoading(true);
 
-            toastMsg(response.data.msg, 'error');
+		//delete
+		axios
+			.delete(`http://localhost:8000/deleteById/${id}`)
+			.then((res) => {
+				if (res.data.status === true) {
+					// window.location.reload();
+					toastMsg('Successfully Deleted.');
+				}
 
-        })
-    }, [])
+				window.location.reload();
+			})
+			.catch((err) => {
+				toastMsg(err.response.data.msg, 'error');
+			});
+		setLoading(false);
+	}
 
-    function handleDelete(id) {
-        setLoading(true);
+	return (
+		<>
+			<Navbar />
+			<div className={styles.mainWrapper}>
+				<h1>My Notes</h1>
 
-        //delete
-        axios.delete(`http://localhost:8000/deleteById/${id}`).then(res => {
-            if (res.data.status === true) {
-                // window.location.reload();
-                toastMsg('Successfully Deleted.');
-            }
+				<Link to='/createNotes'>
+					<button className='btn btn-primary'>Add Notes</button>
+				</Link>
 
-            window.location.reload();
-        }).catch(err => {
-            toastMsg(err.response.data.msg, 'error');
-        })
-        setLoading(false);
+				<div className={styles.notes}>
+					{!loading ? (
+						notes.map((value) => (
+							<Card
+								id={styles.card}
+								style={{ width: '15rem', display: 'flex' }}
+							>
+								<Card.Img
+									variant='top'
+									style={{ width: '250px', height: '200px' }}
+									src={image}
+								/>
+								<Card.Body>
+									<Card.Title>{value.title}</Card.Title>
+									<Card.Text>{value.description}</Card.Text>
+									<div className={styles.button}>
+										<Link to={`/updateNotes/${value._id}`}>
+											<Button variant='primary'>Update</Button>
+										</Link>
 
-    }
-
-    return (
-        <>
-            <Navbar />
-            <div className={styles.mainWrapper}>
-                <h1>My Notes</h1>
-
-                <Link to='/createNotes'>
-                    <button className='btn btn-primary'>Add Notes</button>
-                </Link>
-
-
-                <div className={styles.notes}>
-
-                    {!loading ? notes.map((value) => (
-                        <Card id={styles.card} style={{ width: '15rem', display: "flex" }}>
-                            <Card.Img variant="top" style={{ width: "250px", height: "200px" }} src={image} />
-                            <Card.Body>
-                                <Card.Title>{value.title}</Card.Title>
-                                <Card.Text>{value.description}
-                                </Card.Text>
-                                <div className={styles.button}>
-                                    <Link to={`/updateNotes/${value._id}`}><Button variant="primary">Update</Button></Link>
-
-                                    <Button style={{ marginLeft: "1rem" }} variant="danger" onClick={() => handleDelete(value._id)}>Delete</Button>
-                                </div>
-
-                            </Card.Body>
-                        </Card>
-                    )) : <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", marginTop: "100px" }}>
-                        <Spinner animation="border" variant="primary" />
-                    </div>}
-                </div>
-            </div>
-
-        </>
-    )
+										<Button
+											style={{ marginLeft: '1rem' }}
+											variant='danger'
+											onClick={() => handleDelete(value._id)}
+										>
+											Delete
+										</Button>
+									</div>
+								</Card.Body>
+							</Card>
+						))
+					) : (
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								width: '100%',
+								marginTop: '100px',
+							}}
+						>
+							<Spinner animation='border' variant='primary' />
+						</div>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default AddNotes;
