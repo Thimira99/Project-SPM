e5238bab15147ff2478513375c7e645cf6b8a9da
IT20005580 <88153854+Thimira99@users.noreply.github.com>
2022-10-15 13:52:39 +0530

    add redirectios

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
@@ -13,116 +12,110 @@ import axios from 'axios';
 import login from './Login.module.scss';
 
 function Login() {
+	//use History
+	const history = useHistory();
 
-    //use History
-    const history = useHistory();
+	//initializing variables
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [status, setStatus] = useState(true);
 
-    //initializing variables
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [status, setStatus] = useState(true);
+	//handle submit
+	function handleSubmit(e) {
+		e.preventDefault();
 
-    //handle submit
-    function handleSubmit(e) {
-        e.preventDefault();
+		//set email and password to data
+		const data = {
+			email,
+			password,
+		};
+		console.log('dataaa', data);
+		//login post
+		axios
+			.post('http://localhost:8000/login', data)
+			.then((res) => {
+				toastMsg('Login success.');
 
-        //set email and password to data
-        const data = {
-            email,
-            password
-        };
-        console.log("dataaa", data)
-        //login post
-        axios.post("http://localhost:8000/login", data).then((res) => {
+				console.log(res.data.data);
+				//set token to a variable
+				const token = res.data.data;
 
-            toastMsg('Login success.');
+				//store user token in localstorage
+				localStorage.setItem('user', JSON.stringify(jwt(token)));
+				localStorage.setItem('Token', token);
 
-            console.log(res.data.data);
-            //set token to a variable
-            const token = res.data.data;
+				const { status, accountType } = jwt(token);
 
-            //store user token in localstorage
-            localStorage.setItem('user', JSON.stringify(jwt(token)));
-            localStorage.setItem('Token', token);
-         
+				if (status === false) {
+					history.push('/updateEmployee');
+				} else {
+					if (accountType === 'admin') {
+						history.push('/mainPage');
+					} else if (accountType === 'Sales Rep') {
+						history.push('/salesPerson');
+					} else if (accountType === 'Stock Manager') {
+						history.push('/stockManagement');
+					} else if (accountType === 'Production Manager') {
+						history.push('/projectMHome');
+					} else if (accountType === 'Raw Material & Product Manager') {
+						history.push('/productManager');
+					}
+				}
+			})
+			.catch((err) => {
+				//error handling
+				if (err.response.data.status === false) {
+					toastMsg(err.response.data.msg, 'error');
+				}
+			});
+	}
 
-            const { status, accountType } = jwt(token);
+	//handle email onchange
+	function handleEmail(event) {
+		setEmail(event.target.value);
+	}
 
+	//handle password onchange
+	function handlePassword(event) {
+		setPassword(event.target.value);
+	}
 
-            if (status === false) {
-                history.push('/updateEmployee');
-            } else {
-                if (accountType === 'admin') {
-                    history.push("/mainPage");
-                } else if (accountType === "Sales Rep") {
-                    history.push("/salesPerson");
-                } else if (accountType === "Stock Manager") {
-                    history.push("/stockManagement")
-                } else if (accountType === "Production Manager") {
-                    history.push("/productionManager")
-                } else if (accountType === "Raw Material & Product Manager") {
-                    history.push("/productManager")
-                }
-            }
-        }
-        ).catch(err => {
-            //error handling
-            if (err.response.data.status === false) {
-                toastMsg(err.response.data.msg, 'error')
-            }
-        })
-    }
-
-    //handle email onchange
-    function handleEmail(event) {
-        setEmail(event.target.value);
-    }
-
-    //handle password onchange
-    function handlePassword(event) {
-        setPassword(event.target.value);
-    }
-
-    useEffect(() => {
- 
-    localStorage.setItem('user', JSON.stringify(""));
-    
-   
-       
+	useEffect(() => {
+		localStorage.setItem('user', JSON.stringify(''));
 	}, []);
 
-
-    return (
-        <>
-            <div className={login.login_container}>
-                <div className={login.loginform_container}>
-                    <div className={login.form_container}>
-                        <form className={login.form} onSubmit={handleSubmit}>
-                            <h1>LogIn</h1>
-                            <input
-                                type='text'
-                                placeholder='Email'
-                                name='email'
-                                required
-                                onChange={handleEmail}
-                                className={login.input}
-                            />
-                            <input
-                                type='password'
-                                placeholder='Password'
-                                name='password'
-                                required
-                                onChange={handlePassword}
-                                className={login.input}
-                            />
-                            <button type='submit' className={login.greenBtn}>LogIn</button>
-                        </form>
-                    </div>
-                </div>
-            </div >
-
-        </>
-    )
+	return (
+		<>
+			<div className={login.login_container}>
+				<div className={login.loginform_container}>
+					<div className={login.form_container}>
+						<form className={login.form} onSubmit={handleSubmit}>
+							<h1>LogIn</h1>
+							<input
+								type='text'
+								placeholder='Email'
+								name='email'
+								required
+								onChange={handleEmail}
+								className={login.input}
+							/>
+							<input
+								type='password'
+								placeholder='Password'
+								name='password'
+								required
+								onChange={handlePassword}
+								className={login.input}
+							/>
+							<button type='submit' className={login.greenBtn}>
+								LogIn
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Login;
